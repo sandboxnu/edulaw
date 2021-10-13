e08b819af4e69d0ae8a657bb928a5aa696f40a6b
nicolesorial <grace.cola@gmail.com>
2021-10-12 21:30:57 -0400

    adding initial attempt of toolbar

--- edulaw/pages/_app.tsx
+++ edulaw/pages/_app.tsx
@@ -1,7 +1,11 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from '@material-ui/core'
+import { getLightTheme } from '../styles/themes/index'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <ThemeProvider theme={getLightTheme()} >
+    <Component {...pageProps} />
+  </ThemeProvider >
 }
 export default MyApp
