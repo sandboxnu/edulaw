1b90f6e4467aa14bdd049a750caab0835ba6692d
Azucarheart Clues <connietliu2020@gmail.com>
2021-10-17 00:17:52 -0400

    commented todos for sideProgressBar

--- edulaw/components/SideProgressBar.tsx
+++ edulaw/components/SideProgressBar.tsx
@@ -8,6 +8,8 @@ import { withStyles } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { HomeOutlined, PlayCircleFilled } from '@material-ui/icons'; 
 
+// todo: fix console error
+
 const useStyles = makeStyles((theme) => ({
   listItemText: {
     fontSize: "20px",
@@ -30,7 +32,7 @@ function SideProgrssBar() { // todos: (for down the road) -- routing +  upon com
   const classes = useStyles();
   const [selectedIndex, setSelectedIndex] = React.useState(0);
 
-  const handleListItemClick = (event: any, index: number) => { // todo: figure out event type?
+  const handleListItemClick = (event: MouseEvent, index: number) => { 
     setSelectedIndex(index);
   };
 
