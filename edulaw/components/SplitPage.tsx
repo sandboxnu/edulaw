4ea26f463bf8246b4b4bb40c65576109160fadc2
nicolesorial <grace.cola@gmail.com>
2021-10-26 22:22:16 -0400

    add optional center component to splitpage

--- edulaw/components/SplitPage.tsx
+++ edulaw/components/SplitPage.tsx
@@ -12,16 +12,21 @@ const useStyles = makeStyles((theme) => ({
 interface SplitPageProps {
   left: JSX.Element,
   right: JSX.Element
+  center?: JSX.Element,
   leftPercentage?: Number,
   rightPercentage?: Number
+  centerPercentage?: Number
 }
 
-const SplitPage: FC<SplitPageProps> = ({ left, right, leftPercentage = 70, rightPercentage = 30 }): JSX.Element => {
+const SplitPage: FC<SplitPageProps> = ({ left, right, center = <div></div>, leftPercentage = 70, rightPercentage = 30, centerPercentage = 0 }): JSX.Element => {
   const classes = useStyles();
   return (
     <div className={classes.splitPage}>
       <div style={{ width: leftPercentage + '%', height: '100%', position: 'relative' }}>
         {left}
+      </div>
+      <div style={{ width: centerPercentage + '%', height: '100%', position: 'relative' }}>
+        {center}
       </div>
       <div style={{ width: rightPercentage + '%', height: '100%', position: 'relative' }}>
         {right}
