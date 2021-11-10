923d3e7297ef5f13030e78fd5270f9c2639fb79a
Zefeng (Daniel) Wang <wang.zef@husky.neu.edu>
2021-11-09 22:10:24 -0800

    i hate this.

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,9 +2,9 @@ import Logo from './Logo'
 import { AppBar, Toolbar, Divider } from '@material-ui/core'
 import Typography from '@material-ui/core/Typography'
 import { PhoneOutlined } from '@material-ui/icons'
-import { makeStyles } from '@material-ui/core/styles'
+import { makeStyles, Theme } from '@material-ui/core/styles'
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles((theme: Theme) => ({
   toolBar: {
     padding: '0px',
     justifyContent: 'space-between',
