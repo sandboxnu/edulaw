205d03865e77c1964b871d9873289e69008bd4da
chiliPaneer <Smokey1bcd!>
2021-11-13 19:21:42 -0500

    got styled-components working on a test button - so ugly plz dont @me

---
+++ components/FormStyles/Button.tsx
@@ -0,0 +1,10 @@
+import styled, { css } from 'styled-components'
+
+export const Button = styled.button`
+  background: #5064c7;
+  border-radius: 6;
+  color: white;
+  width: 140px;
+  height: 54px;
+  margin: 0 1em;
+`
---
+++ pages/formDev/results.tsx
@@ -0,0 +1,10 @@
+import React, { useContext } from 'react'
+import { FormCtx } from '../../utils/FormContext'
+
+const ExampleResults: React.FC = () => {
+  const { formValues } = useContext(FormCtx)
+
+  return <div>{`Name: ${formValues.firstName} ${formValues.lastName}`}</div>
+}
+
+export default ExampleResults
