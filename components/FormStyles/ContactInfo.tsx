17c3e70a4133bdd9f2e380d979da5f6e90ae919f
Azucarheart Clues <connietliu2020@gmail.com>
2022-02-21 00:23:11 -0500

    styles for the sidebox on login

--- components/FormStyles/ContactInfo.tsx
+++ components/FormStyles/ContactInfo.tsx
@@ -89,9 +89,9 @@ const ContactFormik: React.FC = () => {
     zip: '',
   }
 
-  const defaultFormState: FormContextInterface = {
-    formValues: initValues,
-  }
+  // const defaultFormState: FormContextInterface = {
+  //   formValues: initValues,
+  // }
 
   const spain =
     /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
--- components/Login/SideBox.tsx
+++ components/Login/SideBox.tsx
@@ -1,22 +1,49 @@
 import React from 'react'
 import styled from 'styled-components'
+import Typography from '@material-ui/core/Typography'
+import Link from 'next/link'
 
 // if you have a better name for this pls name it
-// still needs a LOT of styling
 
 export const Container = styled.div`
   width: 340px;
   height: 329px;
   background-color: #ffffff;
   display: flex;
-  flex-direction: column;
+  justify-content: space-evenly;
+  flex-flow: column;
+  align-items: center;
+`
+
+export const TextDiv = styled.div`
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+`
+
+export const HeaderStyle = styled.h2`
+  font-family: Source Sans Pro;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 28px;
+  line-height: 35px;
+  margin-bottom: 4%;
+`
+
+export const SideBoxButton = styled.button`
+  width: 80%;
+  height: 16%;
+  background-color: #ffffff;
+  border: 1px solid #777777;
+  box-sizing: border-box;
+  border-radius: 6px;
 `
 
 interface LoginSignupProps {
   headerPhrase: string
   description: string
   buttonPhrase: string
-  buttonLink?: string
+  buttonLink: string
 }
 
 function SideBox(props: LoginSignupProps) {
@@ -34,9 +61,17 @@ function SideBox(props: LoginSignupProps) {
           fill="#00A39B"
         />
       </svg>
-      <h2>{props.headerPhrase}</h2>
-      <p>{props.description}</p>
-      <button>{props.buttonPhrase}</button>
+      <TextDiv>
+        <HeaderStyle>{props.headerPhrase}</HeaderStyle>
+        <Typography variant="body2">{props.description}</Typography>
+      </TextDiv>
+      <Link href={props.buttonLink} passHref>
+        <SideBoxButton>
+          <Typography variant="button" style={{ textTransform: 'none' }}>
+            {props.buttonPhrase}
+          </Typography>
+        </SideBoxButton>
+      </Link>
     </Container>
   )
 }
