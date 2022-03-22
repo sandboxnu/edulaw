23231b0045663ed443dae617ca20d73a9cb11481
Azucarheart Clues <connietliu2020@gmail.com>
2022-03-22 17:03:42 -0400

    cleaned up landing pages in similar manner to signup pages

---
+++ components/LandingPage/LandingSplitPage.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import styles from '../../styles/Home.module.css'
+import NavBar from '../Critical/NavBar'
+import SplitPage from '../Critical/SplitPage'
+import RightsPrsMenu from '../LandingPage/RightsPrsMenu'
+import StartComplaint from '../LandingPage/StartComplaint'
+
+interface LandingProps {
+  center: JSX.Element
+}
+
+function LandingSplitPage(props: LandingProps) {
+  return (
+    <main className={styles.main}>
+      <NavBar />
+      <SplitPage
+        left={<RightsPrsMenu />}
+        leftStyle={{
+          width: '25%',
+          minHeight: '100vh',
+          position: 'relative',
+          backgroundColor: '#F4F5F7',
+        }}
+        center={props.center}
+        centerStyle={{
+          width: 'calc(75% - 300px)',
+          height: '100%',
+          position: 'relative',
+        }}
+        right={<StartComplaint />}
+        rightStyle={{
+          width: '255px',
+          height: '100%',
+          position: 'sticky',
+          top: '50px',
+          marginTop: '50px',
+        }}
+      />
+    </main>
+  )
+}
+
+export default LandingSplitPage
