6821ad90c8dd2acc252278ede5fcec1a74a581aa
Dillon Scott <dillonscott.2024@gmail.com>
2022-04-10 14:40:33 -0400

    fix requested changes

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -42,46 +42,4 @@ export default NextAuth({
   ],
 })
 
-// "database" look up
-// if (
-//   credentials.username === 'aoun@gmail.com' &&
-//   credentials.password === 'husky'
-// ) {
-//   return {
-//     id: 2,
-//     name: 'Aoun',
-//     email: 'aoun@gmail.com',
-
-// login failed
-/*return null
-},
-}),
-], */
-// callbacks: {
-//   async signIn({ user, profile}) {
-//   const isAllowedToSignIn = true
-
-//   if (isAllowedToSignIn) {
-//     dbConnect()
-//     const db = getDB()
-//       const existingUser = db.collection('user').findOne({username: user.username})
-//       console.log(existingUser)
-//       if(existingUser) {
-//         if(existingUser.password == user.password){
-//           console.log("user existed")
-//           return existingUser;
-//         }
-//       } else{
-//         console.log("user doesn't exist")
-//         return null
-//       }
-//     return true
-//   } else {
-//     // Return false to display a default error message
-//     return false
-//     // Or you can return a URL to redirect to:
-//     // return '/unauthorized'
-//   }
-// }
-
 /* eslint-enable */
--- server/_dbConnect.ts
+++ server/_dbConnect.ts
@@ -17,14 +17,10 @@ export const dbConnect = async (): Promise<MongoClient | undefined> => {
     await client.connect()
     // Establish and verify connection
     await client.db('admin').command({ ping: 1 })
-    // const pet = await client.db('test').collection('pets').findOne()
     console.log('Connected successfully to server')
     return client
-    // console.log(pet)
   } catch (error) {
     console.log(error)
     return undefined
-    // Ensures that the client will close when you finish/error
-    await client.close()
   }
 }
