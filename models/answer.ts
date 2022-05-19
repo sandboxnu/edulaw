7c681b3aaa4b93236ec3055a09b89c6a10e854e2
Dillon Scott <dillonscott.2024@gmail.com>
2022-05-18 21:37:41 -0400

    oops need to delete some more stuff

--- models/answer.ts
+++ models/answer.ts
@@ -1,11 +1,4 @@
-import { answers } from '../constants'
-
-type Answers = typeof answers
-const typedAnswers = answers as Answers
-
-export type AnswersKeys = keyof Answers
 export interface Answer {
   content?: string
   route: number
 }
-export { typedAnswers as answers }
