e273c31b74084ed226ea1d00d187518b2bfba66b
Dillon Scott <dillonscott.2024@gmail.com>
2022-06-14 19:50:59 -0400

    save endpoint connection

--- pages/form/district.tsx
+++ pages/form/district.tsx
@@ -1,9 +1,41 @@
-import React, { useState } from 'react'
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
+import React, { useState, useEffect } from 'react'
 import { districts, schools } from '../../constants'
+import { DistrictDB } from '../api/form/district/save'
 
 const District: React.FC = () => {
   const [district, setDistrict] = useState<number | undefined>(undefined)
   const [school, setSchool] = useState<number | undefined>(undefined)
+  const router = useRouter()
+  const { data, status } = useSession()
+
+  if (status === 'unauthenticated') {
+    router.push('/signup')
+  }
+
+  useEffect(() => {
+    const save = async () => {
+      if (!data?.user?.id) {
+        return
+      }
+      const userID = data.user.id
+      const body: Omit<DistrictDB, '_id'> = {
+        userID: userID,
+        district: district,
+        school: school,
+      }
+      const result = await fetch('/api/form/district/save', {
+        method: 'POST',
+        body: JSON.stringify(body),
+      })
+      const resBody = await result.json()
+      if (result.status !== 200) {
+        console.error(resBody.error)
+      }
+    }
+    save()
+  }, [district, school])
 
   return (
     <div>
