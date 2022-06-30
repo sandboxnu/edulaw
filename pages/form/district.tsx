c6e33742e27f6f1538ca6b5b1889812938496410
Dillon Scott <dillonscott.2024@gmail.com>
2022-06-29 20:33:01 -0400

    loading built into district page

--- pages/form/district.tsx
+++ pages/form/district.tsx
@@ -9,11 +9,14 @@ const District: React.FC = () => {
   const [school, setSchool] = useState<number | undefined>(undefined)
   const router = useRouter()
   const { data, status } = useSession()
+  const [loaded, setLoaded] = useState<boolean>(false)
 
+  // reroutes to signup if not logged in
   if (status === 'unauthenticated') {
     router.push('/signup')
   }
 
+  // saves values to database
   useEffect(() => {
     const save = async () => {
       if (!data?.user?.id) {
@@ -37,7 +40,32 @@ const District: React.FC = () => {
     save()
   }, [district, school])
 
-  return (
+  // loads values in from database, only loads once
+  useEffect(() => {
+    const retrieve = async () => {
+      if (!data?.user?.id) {
+        return
+      }
+      const userID = data.user.id
+      const result = await fetch(`/api/form/district/retrieve?userID=${userID}`)
+      const body = await result.json()
+      if (result.status !== 200) {
+        console.error(body.error)
+      } else {
+        const typedBody = body as DistrictDB
+        setDistrict(typedBody.district)
+        setSchool(typedBody.school)
+      }
+      setLoaded(true)
+    }
+    if (!loaded) {
+      retrieve()
+    }
+  }, [data])
+
+  return !loaded ? (
+    <p>loading...</p>
+  ) : (
     <div>
       <select
         name="District"
