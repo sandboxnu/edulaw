6b2722cae1af04e554f88ebb42437c89e7c8d54c
imanmoreira <moreira.i@husky.neu.edu>
2022-08-01 03:22:11 -0400

    Adding all the stuff but none of the styling

---
+++ pages/api/form/additionalInfo/save.ts
@@ -0,0 +1,38 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { WithId, Document } from 'mongodb'
+import { dbConnect } from '../../../../server/_dbConnect'
+
+export interface AdditionalInfoDb extends WithId<Document> {
+  userID: string
+  relationship: number
+  language: number
+  deseAccommodations: string
+  bsea: boolean
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== 'POST') {
+    res.status(400).json({ error: 'Expected POST request' })
+    return
+  }
+
+  const doc = JSON.parse(req.body) as Omit<AdditionalInfoDb, '_id'>
+  const client = await dbConnect()
+  if (!client) {
+    res.status(500).json({ error: 'Client is not connected' })
+    return
+  }
+  await client.connect()
+  const formCollection = client.db('edlaw').collection('additional')
+  const result = await formCollection.replaceOne({ userID: doc.userID }, doc, {
+    upsert: true,
+  })
+  if (result.acknowledged) {
+    res.status(200).json({ success: true })
+  } else {
+    res.status(401).json({ error: 'An error occurred while saving' })
+  }
+}
