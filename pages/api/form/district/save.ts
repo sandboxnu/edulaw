4b5ba7cb04b993e1dc02baad609f5823b1d7b43a
Dillon Scott <67511512+dillydally414@users.noreply.github.com>
2022-08-08 20:30:15 -0400

    District (#80)
    * school and district page prototype
    
    * save endpoint
    
    * retrieve endpoint
    
    * save endpoint connection
    
    * loading built into district page
    
    * change index to string

---
+++ pages/api/form/district/save.ts
@@ -0,0 +1,36 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { WithId, Document } from 'mongodb'
+import { dbConnect } from '../../../../server/_dbConnect'
+
+export interface DistrictDB extends WithId<Document> {
+  userID: string
+  district: string
+  school: string
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== 'POST') {
+    res.status(400).json({ error: 'Expected POST request' })
+    return
+  }
+
+  const doc = JSON.parse(req.body) as Omit<DistrictDB, '_id'>
+  const client = await dbConnect()
+  if (!client) {
+    res.status(500).json({ error: 'Client is not connected' })
+    return
+  }
+  await client.connect()
+  const formCollection = client.db('edlaw').collection('district')
+  const result = await formCollection.replaceOne({ userID: doc.userID }, doc, {
+    upsert: true,
+  })
+  if (result.acknowledged) {
+    res.status(200).json({ success: true })
+  } else {
+    res.status(401).json({ error: 'An error occurred while saving' })
+  }
+}
